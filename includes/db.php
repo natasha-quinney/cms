49db4b3d6f619dfedd6103fc66d52717a6b2446c
Natasha Quinney <natasha.quinney@inkyzilla.com>
2018-01-18 17:22:26 +1100

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$db['db_host'] = "localhost";
+$db['db_username'] = "root";
+$db['db_password'] = "root";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value){
+    
+    define(strtoupper($key),$value);
+    
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//if($connection){
+//    
+//    echo "Connected to database";
+//    
+//}
+
+?>
