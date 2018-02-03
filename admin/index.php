f8614c0bfc707b8a4db0c81ffe85277a82611cba
Natasha Quinney <natasha.quinney@inkyzilla.com>
2018-02-03 11:02:55 +1100

    Added count widgets and links to dashboard.

--- admin/index.php
+++ admin/index.php
@@ -15,8 +15,6 @@
                 <div class="row">
                     <div class="col-lg-12">
                        
-                       
-                       
                         <h1 class="page-header">
                             welcome to admin
                             <small><?php echo $_SESSION['username']; ?></small>
@@ -34,6 +32,141 @@
                     </div>
                 </div>
                 <!-- /.row -->
+                
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                    
+                                    <?php 
+                                        
+                                        $query = "SELECT * FROM posts";
+                                        $select_posts = mysqli_query($connection, $query);
+                                        
+                                        $post_count = mysqli_num_rows($select_posts);
+                                        
+                                        echo "<div class='huge'>{$post_count}</div>";
+                                        
+                                        ?>
+                                  
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="posts.php?source=view_post">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                    
+                                     <?php 
+                                        
+                                        $query = "SELECT * FROM comments";
+                                        $select_comments = mysqli_query($connection, $query);
+                                        
+                                        $comment_count = mysqli_num_rows($select_comments);
+                                        
+                                        echo "<div class='huge'>{$comment_count}</div>";
+                                        
+                                        ?>
+                                        
+                                      <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                    
+                                     <?php 
+                                        
+                                        $query = "SELECT * FROM users";
+                                        $select_users = mysqli_query($connection, $query);
+                                        
+                                        $user_count = mysqli_num_rows($select_users);
+                                        
+                                        echo "<div class='huge'>{$user_count}</div>";
+                                        
+                                        ?>
+                                  
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php?source=view_users">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                       
+                                        <?php 
+                                        
+                                        $query = "SELECT * FROM categories";
+                                        $select_categories = mysqli_query($connection, $query);
+                                        
+                                        $category_count = mysqli_num_rows($select_categories);
+                                        
+                                        echo "<div class='huge'>{$category_count}</div>";
+                                        
+                                        ?>
+                            
+                                         <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="categories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
             </div>
             <!-- /.container-fluid -->
